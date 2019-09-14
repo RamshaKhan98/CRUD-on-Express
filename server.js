4856db9378da5acd3223ee9d29796b5f0d721e27
Ramsha <ramsha_khan-98@hotmail.com>
2019-09-15 01:17:14 +0500

    Enabling CORS

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const bodyParser=require('body-parser');
 const con = require('./config/database')
 const heroRoutes=require('./routes//hero')
 const app=express();
+const cors = require('cors')
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 204 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+
+app.use(cors(corsOptions))
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
